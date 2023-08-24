5ba07968c37696fa88f6f6de40e7dee4f5c664dd
chetan <chetansdk@gmail.com>
2023-08-24 08:46:55 +0200

    added teams

--- src/test/java/live/scoreboard/ScoreBoardTest.java
+++ src/test/java/live/scoreboard/ScoreBoardTest.java
@@ -1,5 +1,6 @@
 package live.scoreboard;
 
+import live.scoreboard.football.FootballTeam;
 import live.scoreboard.football.ScoreBoard;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,8 @@ public class ScoreBoardTest {
 
     @Test
     void shouldAddMatch() {
+        FootballTeam hometeam = new FootballTeam();
+        FootballTeam awayTeam = new FootballTeam();
         scoreBoard = new ScoreBoard();
     }
 
