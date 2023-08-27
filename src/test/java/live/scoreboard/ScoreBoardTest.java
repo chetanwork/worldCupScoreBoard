7928588ede90271d80c526c710b45ba986569264
chetan <chetansdk@gmail.com>
2023-08-27 15:46:38 +0200

    test cases for update match

--- src/test/java/live/scoreboard/ScoreBoardTest.java
+++ src/test/java/live/scoreboard/ScoreBoardTest.java
@@ -74,12 +74,51 @@ public class ScoreBoardTest {
     }
 
     @Test
-    void shouldUpdateMatch() {
+    void checkDefaultScoreAsZero() {
         scoreBoard = new ScoreBoardImpl();
+        FootballTeam homeTeam = new FootballTeam("Mexico");
+        FootballTeam awayTeam = new FootballTeam("Canada");
+        scoreBoard.shouldAddMatch(homeTeam, awayTeam);
+        assertEquals(0, homeTeam.getScore());
+        assertEquals(0, homeTeam.getScore());
     }
 
+    @Test
+    void shouldNotUpdateMatchScoreWithNegative() {
+        scoreBoard = new ScoreBoardImpl();
+        FootballTeam homeTeam = new FootballTeam("Mexico");
+        FootballTeam awayTeam = new FootballTeam("Canada");
+        scoreBoard.shouldAddMatch(homeTeam, awayTeam);
+        GeneralException exception = assertThrows(GeneralException.class, () ->
+            scoreBoard.shouldUpdateScore(homeTeam, awayTeam, -2, 1), "Negative Score cannot be update");
+        String expectedErrorMessage = "Negative Score cannot be update";
+        assertEquals(expectedErrorMessage, exception.getMessage());
+    }
 
+    @Test
+    void shouldUpdateMatchScore() {
+        scoreBoard = new ScoreBoardImpl();
+        FootballTeam homeTeam = new FootballTeam("Mexico");
+        FootballTeam awayTeam = new FootballTeam("Canada");
+        scoreBoard.shouldAddMatch(homeTeam, awayTeam);
+        scoreBoard.shouldUpdateScore(homeTeam, awayTeam, 3, 4);
+        assertEquals(3, homeTeam.getScore());
+        assertEquals(4, awayTeam.getScore());
+    }
 
-
+    @Test
+    void throwErrorWhenUpdateMatchIsNotFound() {
+        scoreBoard = new ScoreBoardImpl();
+        FootballTeam homeTeam = new FootballTeam("Mexico");
+        FootballTeam awayTeam = new FootballTeam("Canada");
+        FootballTeam homeTeam1 = new FootballTeam("Germany");
+        FootballTeam awayTeam1 = new FootballTeam("France");
+        scoreBoard.shouldAddMatch(homeTeam, awayTeam);
+        scoreBoard.shouldUpdateScore(homeTeam, awayTeam, 1,0);
+        GeneralException exception = assertThrows(GeneralException.class, () ->
+            scoreBoard.shouldUpdateScore(homeTeam1, awayTeam1, 1,2), "Match Not Found To Update Score");
+        String expectedErrorMessage = "Match Not Found To Update Score";
+        assertEquals(expectedErrorMessage, exception.getMessage());
+    }
 
 }
